dc86872d5dae4c6550546f4c205967f8325eb38f
EzizmuhammetAshyrmuhammedow <emuhammedow1@gmail.com>
2025-02-25 22:10:47 +0500

    Working state

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,5 +1,7 @@
 import "../application";
 import {Alpine} from "alpinejs";
+import "flatpickr/dist/flatpickr.min.css"
+import "flatpickr/dist/themes/dark.css"
 
 window.Alpine = Alpine;
 Alpine.start();
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,4 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.precompile += %w[ flatpickr-light.css flatpickr-dark.css bs-icons.css ]
